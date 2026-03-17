9fe4ab6a0e3d0fc11105f7472f67ab0f8cb2d97c
whdkfk <joara080827@gmail.com>
2026-03-17 23:07:40 +0900

    feat/#21 :: url수정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const backendUrl = process.env.NEXT_PUBLIC_API_URL ?? "https://ai-production-e966.up.railway.app";
+const backendUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://be-production-4099.up.railway.app";
 
 const nextConfig: NextConfig = {
   async rewrites() {
